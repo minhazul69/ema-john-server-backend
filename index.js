69d2154f09a75c84b82d7f41d8a3ff97d50d6c9c
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-04-25 14:41:02 +0600

    load data

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -9,6 +11,28 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("my ema john website is running");
 });
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6qty1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("emaJohn").collection("product");
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 app.listen(port, () => {
   console.log("port listening", port);
 });
